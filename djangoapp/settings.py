30d1bbd787f466234b2b7a906bd6179c3307b0a5
mokhtarjr74 <ahmed.mokhtar.jr@gmail.com>
2024-01-11 21:55:41 +0200

    adding redis cofiguration

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis-master:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
